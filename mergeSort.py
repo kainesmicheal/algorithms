3204c4c28df868c66daca8b72960385c5567606e
Micheal <38526063+kainesmicheal@users.noreply.github.com>
2019-12-04 12:32:05 +0530

    Update mergeSort.py

--- mergeSort.py
+++ mergeSort.py
@@ -6,13 +6,13 @@ def mergeSort(arr, startIndex, endIndex):
         merge(arr,startIndex,middleElm,endIndex)
         
 
-def merge(arr, s,m,e):
-     left = arr[s:m]
-     right = arr[m:e]
-     k = s 
+def merge(arr, start,middle,end):
+     left = arr[start:middle]
+     right = arr[middle:end]
+     k = start
      i = 0
      j = 0
-     while (s + i < m and m + j < e):
+     while (start + i < middle and middle + j < end):
         if(left[i] <= right[j]):
              arr[k] = left[i]
              i = i + 1
@@ -20,12 +20,12 @@ def merge(arr, s,m,e):
              arr[k] = right[j]
              j =j + 1 
         k = k + 1 
-     if s + i < m:
-         while k < e:
+     if start + i < middle:
+         while k < end:
              arr[k] = left[i]
              i = i + 1 
              k = k + 1
-         while k < e:
+         while k < end:
              arr[k] = right[j]
              j = j + 1 
              k = K + 1
@@ -33,7 +33,4 @@ arr = [12,14,11,5,8,6]
 n = len(arr)
 mergeSort(arr, 0, n)
 print(arr)
-     
-     
-     
      
