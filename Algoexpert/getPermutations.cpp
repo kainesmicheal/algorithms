71cec5982cec1d332a0b913b8a4da83048cd2fe7
Micheal <38526063+kainesmicheal@users.noreply.github.com>
2020-09-19 20:56:06 +0530

    Update getPermutations.cpp

--- Algoexpert/getPermutations.cpp
+++ Algoexpert/getPermutations.cpp
@@ -30,3 +30,32 @@ void swap(int i,int j,vector<int>& array){
 }
 //time complexity is o(n! * n)
 //space compelxity is 0(n! * n)
+
+#include <vector>
+using namespace std;
+void permutationsHelper(vector<int> array, vector<int> currentPermutation,vector<vector<int>>* permutations);
+
+vector<vector<int>> getPermutations(vector<int> array) {
+  vector<vector<int>> permutations;
+	permutationsHelper(array,{},&permutations);
+  return permutations;
+}
+
+void permutationsHelper(vector<int> array,vector<int> currentPermutation,
+												vector<vector<int>>* permutations)
+{
+	if(array.size() == 0 && currentPermutation.size() > 0) {
+		permutations->push_back(currentPermutation);
+	}	else {
+		for (int i = 0; i<array.size(); i++) {
+			vector<int> newArray;
+			newArray.insert(newArray.end(), array.begin(), array.begin() + i);
+			newArray.insert(newArray.end(), array.begin()+i+1, array.end());
+			vector<int> newPermutation = currentPermutation;
+			newPermutation.push_back(array[i]);
+			permutationsHelper(newArray, newPermutation, permutations);
+		}
+	}
+}
+//time complexity is o(n^2 * n!)
+//space complexity is o(n! * n)
